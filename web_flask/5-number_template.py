71a2289d4c4bf634ba40c018f384bb38caac36df
imenmansouri93 <imennmansourii@gmail.com>
2023-04-13 23:23:10 +0100

    new

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def Cisfun(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def Pythoniscool(text="is cool"):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def sitanumber(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def Numbertemplate(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
