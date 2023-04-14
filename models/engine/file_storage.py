f62ff6868646f1201a10a11326ec44427a0e7fff
imenmansouri93 <imennmansourii@gmail.com>
2023-04-14 22:25:51 +0100

    new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        new = {}
+        for key, value in FileStorage.__objects.items():
+            if cls  == value.__class__ or cls == value.__class__.__name__:
+                new[key] = value
+            return new
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +54,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    def delete(self, obj=None):
+        """remove objects"""
+        if obj is None:
+            return
+    
+    def close(self):
+
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, Sequence, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City",  backref="state")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """"returns the list of City instances"""
+            listofcities = []
+            allcititesinstance = models.storage.all(City)
+            for i in allcititesinstance.values():
+                if i.state_id == self.id:
+                    listofcities.append(i)
+            return listofcities
